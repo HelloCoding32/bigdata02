2974829baa14f96cd7b15ce800a039945b04a206
HelloCoding32 <user.email>
2025-09-04 15:20:20 +0900

    v0

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -1,14 +1,14 @@
-# Numpy
-# 써드파티 라이브러리 (별도의 설치가 필요, 단 colab에 기본 설치가 되어 있음.)
-# 파이썬에서 수치 계산을 위한 핵심 라이브러리, 다차원 배열 객체와 이를 다루기 위한 다양한 함수를 제공
-
 import numpy as np
 
-# 리스트 대신 배열을 사용하는 이유
-list1 = [1,2,"3",4,5]
-arr1 = np.array(list1) # 파이썬 리스트를 넘파이 배열로 변환
-print(list1, type(list1))
-print(arr1, type(arr1))
+zeros = np.zeros((3,4))
+# zeros = np.array([[0, 0,0,0],[0, 0,0,0],[0, 0,0,0]], dtype=float)
+ones = np.ones((3,5))
+print(zeros)
+print(ones)
 
-arr2 = np.array([[1,2,3],[4,5,6]])
-print(arr2, type(arr2))
+# range_array = np.arange(0,20,2)
+range_array = np.arange(1,10)
+print(range_array)
+
+space_array = np.linspace(0,1,5) # 0부터 1까지 5개로 구간 설정
+print(space_array)
