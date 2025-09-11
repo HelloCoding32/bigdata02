fc2b5c578cf40a7afc4eca305538c1fbe933950a
HelloCoding32 <user.email>
2025-09-11 14:34:24 +0900

    Pandas v0.1

---
+++ week03/week03_pandas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df1 = pd.DataFrame({'A':[1,2,3], 'B':[4,5,6],'C':[7,8,9]}, index=[1,2,3])
+
+# print(df1)
+# print()
+
+df2 = pd.DataFrame([
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+], index =[1, 2, 3], columns=['A', 'B', 'C'])
+# print(df2)
+
+df3 = pd.melt(df2).rename(columns={'variable' : 'var', 'value' : 'val'}).query('val  > 5')
+print(df3)
+
+
