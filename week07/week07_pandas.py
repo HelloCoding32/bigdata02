e4a5a5c586b6b0af4992beac647987e81b35bdb5
HelloCoding32 <user.email>
2025-09-18 19:01:25 +0900

    v1.5 pandas

--- week07/week07_pandas.py
+++ week07/week07_pandas.py
@@ -1,31 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-df = pd.read_csv('savant_data.csv')
-# print(df['player_name'])
-df_new = df[['player_id','player_name','total_pitches','velocity','hrs','so','bb']]
-# print(df_new.query('player_name == "Ohtani, Shohei"'))
-# print(df_new[df_new['player_name'] == 'Ohtani, Shohei'])
+print(len(sns.get_dataset_names()))
 
-# print(df_new.sort_values('velocity',ascending=False))
-# print(df_new.sort_values('so',ascending=False))
+titanic = sns.load_dataset('titanic')
+# print(titanic.head())
 
-# print(df_new.query('player_name == "Ohtani, Shohei"'))
-# print(df_new.describe())
+# print(titanic.describe())
 
-# df1_new = len(df_new[df_new['total_pitches'] > 1000])
-# print(df1_new)
+# print(titanic[titanic['age'] <= 15])
 
-df1_new = df_new[df_new['total_pitches'] > 1000]
-print(df1_new['total_pitches'].count())
-# plt.figure(figsize = (10,5))
-# plt.boxplot()
-# plt.show()
-
-# plt.title('Ohtani, Shohei', fontsize=20, fontweight='bold')
-# plt.xlabel('player_id', fontsize=15)
-# plt.ylabel('total_pitches', fontsize=15)
-# plt.xticks(fontsize=15)
-# plt.yticks(fontsize=15)
-# plt.show()
+print(titanic.nlargest(5, 'age'))
