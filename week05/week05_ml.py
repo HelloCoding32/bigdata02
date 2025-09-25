abd348c6555c05a64f9f4eec0d7cc8ef62705738
HelloCoding32 <feeling0706@naver.com>
2025-09-25 16:40:35 +0900

    leaning

--- week05/week05_ml.py
+++ week05/week05_ml.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
+# from sklearn.linear_model import LinearRegression
+# from sklearn.neighbors import KNeighborsRegressor
+import tglearn as tg
 
 # Download and prepare the data
 df = pd.read_csv("lifesatisfaction.csv")
@@ -20,14 +21,15 @@ y = df[["Life satisfaction"]].values
 # plt.show()
 
 # Select a linear model
-model1 = LinearRegression()
-model2 = KNeighborsRegressor(3)
+# model1 = LinearRegression()
+# model2 = KNeighborsRegressor(3)
+
+model1 = tg.LinearRegression()
+model2 = tg.KNeighborsRegressor()
 # Train the model
 model1.fit(X, y)
 model2.fit(X, y)
 # Make a prediction for Cyprus
 new_instance = [[31_721.30]] # South Korea GDP per capita in 2020
-print(f"Life satisfaction(LinearRegression) : {model1.score(X, y)}")
-print(f"Life satisfaction(KNeighborsRegressor) : {model2.score(X, y)}")
 print(f"예측된 값: {model1.predict(new_instance)[0][0]:.2f}") # output: [[6.30165767]]
 print(f"예측된 값: {model2.predict(new_instance)[0][0]:.2f}") # output: [[5.89940454]]
