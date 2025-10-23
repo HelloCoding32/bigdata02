1dd17d0a3d240cb2fc675a8286e577499b4ac5e1
HelloCoding32 <feeling0706@naver.com>
2025-10-23 16:31:21 +0900

    visualization

--- week09/week_pandas.py
+++ week09/week_pandas.py
@@ -1,21 +1,19 @@
 import pandas as pd
+import seaborn as sns
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
 
 uber = pd.read_csv("Uber.csv")
-# uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'])
-# print(uber)
-
 uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'], errors='coerce')
 uber['END_DATE*'] = pd.to_datetime(uber['END_DATE*'])
-
 uber.drop(1155, inplace=True)
-# print(uber['END_DATE*'].unique())
-# print(uber[uber['MILES*'] == uber['MILES*'].max()])
-
-# print(uber['PURPOSE*'].value_counts())
-# print(uber['PURPOSE*'].isna().sum())
-
-
-# uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*'])
 uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*']).dt.total_seconds() / 60
-# print(uber['DURATION*'])
-print(uber.info())
+slope, intercept, r, _, _ = linregress(uber['MILES*'], uber['DURATION*'])
+print(slope)
+print(intercept)
+print(r)
+
+
+sns.regplot(x='MILES*', y='DURATION*', data=uber, line_kws={'label': f'y={slope:.2f}, R^2={r:.2f}'})
+plt.legend()
+plt.show()
