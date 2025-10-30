c6f9881e30280437935a5e1d3b43a52b136876eb
HelloCoding32 <feeling0706@naver.com>
2025-10-30 15:34:32 +0900

    pandas qcut

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -4,8 +4,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 german = pd.read_csv('german.csv')
-# print(pd.cut(german['Age'], bins=8).reset_index().groupby('Age').count())
-ages = [10,20,30,40,50,60,70,80]
-# print(pd.cut(german['Age'], bins=ages).reset_index().groupby('Age').count())
-print(pd.cut(german['Age'], bins=ages, right=False).reset_index().groupby('Age').count())
-# print(german[german['Age'] < 20])
+print()
+print(pd.qcut(german['Age'], q=8).reset_index().groupby('Age').size())
