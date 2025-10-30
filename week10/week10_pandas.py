02e736b752e37d8405e8edfbd091ab923ffcda1a
HelloCoding32 <feeling0706@naver.com>
2025-10-30 16:25:57 +0900

    visualization

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -1,14 +1,10 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-s1 = pd.Series([0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1])
-print(s1)
-sc1 = s1.cumsum() # Cumulative sum
-# print(sc1)
-# print(s1.mul(sc1)) # multiply
-# print(s1.mul(sc1).diff()) # difference (빼기)
-# print(s1.mul(sc1).diff().where(lambda n: n>0)) # where
-# print(s1.mul(sc1).diff().where(lambda n: n>0).ffill()) # front fill 앞 값을 Nan값에 채우다.
-print(s1.mul(sc1).diff().where(lambda n: n>0).ffill().add(sc1, fill_value=0)) # add 0
+# 다음주 apple 주가 데이터셋 하기 라고 말하기
+
+wh = pd.read_csv('wh.csv')
+# print(wh.describe())
+sns.scatterplot(x='Weight',y='Height',data=wh)
+plt.show()
