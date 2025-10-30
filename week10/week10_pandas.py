f7cbc3e04899695986e3371763862811349e197e
HelloCoding32 <feeling0706@naver.com>
2025-10-30 15:58:30 +0900

    where, ffill, add, difference, multiply

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-german = pd.read_csv('german.csv')
-print()
-print(pd.qcut(german['Age'], q=8).reset_index().groupby('Age').size())
+s1 = pd.Series([0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1])
+print(s1)
+sc1 = s1.cumsum() # Cumulative sum
+# print(sc1)
+# print(s1.mul(sc1)) # multiply
+# print(s1.mul(sc1).diff()) # difference (빼기)
+# print(s1.mul(sc1).diff().where(lambda n: n>0)) # where
+# print(s1.mul(sc1).diff().where(lambda n: n>0).ffill()) # front fill 앞 값을 Nan값에 채우다.
+print(s1.mul(sc1).diff().where(lambda n: n>0).ffill().add(sc1, fill_value=0)) # add 0
