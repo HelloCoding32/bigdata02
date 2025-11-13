8a6e9005298f4c8bf54d047b64c67311c04ce231
HelloCoding32 <feeling0706@naver.com>
2025-11-13 14:16:37 +0900

    pandas

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -5,6 +5,8 @@ import seaborn as sns
 # 다음주 apple 주가 데이터셋 하기 라고 말하기
 
 wh = pd.read_csv('wh.csv')
-print(wh.describe())
+# print(wh.describe())
+# print(wh.query('Weight > 350'))
+print(wh[wh['Weight'] > 390])
 # sns.scatterplot(x='Weight',y='Height',data=wh)
 # plt.show()
