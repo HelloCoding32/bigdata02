3d9f93cad2326c341c17211fec0c19ae14a3b577
HelloCoding32 <feeling0706@naver.com>
2025-11-27 15:34:10 +0900

    check the fail value error

--- week14/week14_pandas.py
+++ week14/week14_pandas.py
@@ -3,35 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-pi = pd.read_csv('product_inspection.csv')
-# print(pi.info())
-# print(pi.describe())
+pr = pd.read_csv('product.csv')
+# print(pr.info())
+# print(pr.head())
+# print(pr.tail())
+# print(pr.describe())
+# print(pr['operator'].unique())
+# print(pr['process'].unique())
+# print(pr['factory'].unique())
 
-pi['date'] = pd.to_datetime(pi['date'])
-# print(pi.info())
-# print(pi[pi['inspection_step'] == 'B'])
-# print(pi['inspection_step'].unique())
-# print(pi.groupby('inspection_step')['value'].mean())
-# print(pi.groupby('inspection_step')['value'].describe())
+pr['path'] = pr.groupby('product_id')['operator'].transform(
+    lambda x : '_'.join(x)
+)
+# print(pr.head(6))
+# print(pr.tail(6))
 
-# 새로운 특성 추가
-pi['standard1'] = pi.groupby('inspection_step')['value'].transform(lambda x: (x - x.mean())/ x.std())
-# print(pi.info())
-# print(pi.tail(5))
-# print(pi.groupby('inspection_step')['value'].mean() == 0)
-
-# print(pi[pi['standard1'] < 0])
-
-temp = pi.sort_values(['inspection_step','date']).drop_duplicates(['inspection_step'])
-# print(temp)
-temp = temp.set_index('inspection_step')['value']
-# print(temp)
-
-pi = pi.set_index('inspection_step')
-# print(pi)
-
-pi['standard2'] = pi['value'] - temp
-# print(pi['standard2'])
-
-pi = pi.reset_index()
-print(pi)
+pr['path'] = pr['factory'] + '_' + pr['path']
+# print(pr.head(6))
+pr = pr.drop_duplicates('product_id')
+# print(pr)
+pr = pr[['date','product_id','passfail','path']]
+print(pr)
+print(pr.groupby('passfail')['path'].value_counts())
